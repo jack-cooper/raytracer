08d0abd8c0bd73b4d55e8729ff7148f5c92306cd
jack-cooper <cooper.jackwilliam@gmail.com>
2022-08-07 15:06:06 +0100

    chore(main): Doubled resolution
    Paralell output means this still finishes in under 1 second 😎

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use ray::Ray;
 use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use std::{io::Write, sync::Arc};
 
-const IMAGE_WIDTH: u64 = 256;
+const IMAGE_WIDTH: u64 = 512;
 const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / Camera::ASPECT_RATIO) as u64;
 
 const MAX_DEPTH: u8 = 5;
